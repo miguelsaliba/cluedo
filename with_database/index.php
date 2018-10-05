b8be3218a5a6a24c1382990b68c3856718bd198e
miguelsaliba <miguosaliba@gmail.com>
2018-10-05 13:07:56 +0400

    i dont remeber what i changed

---
+++ with_database/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <form class="" action="process.php" method="post">
+            Name: <input type="text" name="name" value="" placeholder="Name"><br />
+            Password: <input type="password" name="pass" placeholder="Password"><br />
+            <input type="submit" name="" value="login">
+        </form>
+    </div>
+    <table class="hide">
+        <tr id="title">
+            <th></th>
+        </tr>
+
+
+        <tr class="title">
+            <th class="subtitle">People</th>
+        </td>
+        <tr>
+            <td>Col. Mustard</td>
+        </tr>
+        <tr>
+            <td>Prof. Plum</td>
+        </tr>
+        <tr>
+            <td>Mr. Green</td>
+        </tr>
+        <tr>
+            <td>Mrs. Peacock</td>
+        </tr>
+        <tr>
+            <td>Miss Scarlett</td>
+        </tr>
+        <tr>
+            <td>Mrs. White</td>
+        </tr>
+
+
+        <tr class="title">
+            <th class="subtitle">Weapons</th>
+        </tr>
+        <tr>
+            <td>Dagger</td>
+        </tr>
+        <tr>
+            <td>Candle Stick</td>
+        </tr>
+        <tr>
+            <td>Revolver</td>
+        </tr>
+        <tr>
+            <td>Rope</td>
+        </tr>
+        <tr>
+            <td>Lead Pipe</td>
+        </tr>
+        <tr>
+            <td>Spanner</td>
+        </tr>
+
+
+        <tr class="title">
+            <th class="subtitle">Rooms</th>
+        </tr>
+        <tr>
+            <td>Hall</td>
+        </tr>
+        <tr>
+            <td>Lounge</td>
+        </tr>
+        <tr>
+            <td>Dining Room</td>
+        </tr>
+        <tr>
+            <td>Kitchen</td>
+        </tr>
+        <tr>
+            <td>Ball Room</td>
+        </tr>
+        <tr>
+            <td>Conservatory</td>
+        </tr>
+        <tr>
+            <td>Billiard Room</td>
+        </tr>
+        <tr>
+            <td>Library</td>
+        </tr>
+        <tr>
+            <td>Study</td>
+        </tr>
+    </table>
+
+<script src="main.js"></script>
+
+</body>
+</html>
